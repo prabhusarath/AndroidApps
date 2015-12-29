c34e7a9770be4cbfccdef0e6a852e6673f29b7dd
saddy1923 <prabhu.sarath@gmail.com>
2015-12-29 00:19:53 -0600

    Up

--- ToastDisplay/app/src/main/java/com/example/sarathkumar/toastdisplay/MainActivity.java
+++ ToastDisplay/app/src/main/java/com/example/sarathkumar/toastdisplay/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         EditText textf = (EditText) findViewById(R.id.t1);
 
-        Log.i("******************", textf.getText().toString());
+        //Log.i("******************", textf.getText().toString());
 
         //Toast.makeText(getApplication(),"textf.getText().toString()",Toast.LENGTH_LONG).show();
 
