75eaea6aa483271e9342c1fa080aa3d5e2943203
saddy1923 <prabhu.sarath@gmail.com>
2015-12-30 18:35:09 -0600

    Audio

--- AudioNew/app/src/main/java/com/example/sarathkumar/audionew/MainActivity.java
+++ AudioNew/app/src/main/java/com/example/sarathkumar/audionew/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sarathkumar.audionew;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,17 +11,58 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.media.MediaPlayer;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
 
     MediaPlayer mPlay;
+    AudioManager audio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        mPlay = MediaPlayer.create(this, R.raw.laugh);
+
+        audio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        int Max = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int Cur = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        SeekBar Vol = (SeekBar) findViewById(R.id.seekBar);
+
+        Vol.setMax(Max);
+        Vol.setProgress(Cur);
+
+
+        Vol.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //  Log.i("**********",Integer.toString(progress));
+
+                audio.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+        });
 
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -30,18 +73,68 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        final SeekBar scrub = (SeekBar) findViewById(R.id.Scrub);
+        scrub.setMax(mPlay.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scrub.setProgress(mPlay.getCurrentPosition());
+            }
+        },0,1000);
+
+        scrub.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                mPlay.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     public void play(View view)
     {
-        mPlay = MediaPlayer.create(this,R.raw.laugh);
+
         mPlay.start();
+
     }
 
 
     public void pause(View view)
     {
-        mPlay.pause();
+        mPlay.stop();
     }
 
 
