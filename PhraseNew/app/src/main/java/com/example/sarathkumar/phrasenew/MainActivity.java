16f0dc51108173501727f3793e8cb2af6ca59e85
saddy1923 <prabhu.sarath@gmail.com>
2015-12-31 14:29:05 -0600

    New

--- PhraseNew/app/src/main/java/com/example/sarathkumar/phrasenew/MainActivity.java
+++ PhraseNew/app/src/main/java/com/example/sarathkumar/phrasenew/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         MediaPlayer mplay = MediaPlayer.create(this,resid);
         mplay.start();
-        
+
         //Log.i("***********",check);
     }
 
