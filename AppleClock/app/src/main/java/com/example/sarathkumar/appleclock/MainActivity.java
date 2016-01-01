5c574b6f0eac5f6e5d29dc6c984b64650a335859
saddy1923 <prabhu.sarath@gmail.com>
2015-12-31 21:00:05 -0600

    Clock

---
+++ AppleClock/app/src/main/java/com/example/sarathkumar/appleclock/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.sarathkumar.appleclock;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar s1;
+    TextView t1;
+
+
+    public void check(int p)
+    {
+        int min = (int) p/60;
+        int sec = p - min * 60;
+        t1.setText(Integer.toString(min)+":"+Integer.toString(sec));
+    }
+    public void timer(View view)
+    {
+        new CountDownTimer(s1.getProgress()*1000+100, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                check((int) millisUntilFinished/1000);
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                t1.setText("0:00");
+                MediaPlayer mPlay = MediaPlayer.create(getApplicationContext(),R.raw.tv);
+
+                mPlay.start();
+
+            }
+        }.start();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        s1 = (SeekBar) findViewById(R.id.s1);
+        t1 = (TextView) findViewById(R.id.t1);
+        
+        s1.setMax(600);
+        s1.setProgress(30);
+
+        s1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            check(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
