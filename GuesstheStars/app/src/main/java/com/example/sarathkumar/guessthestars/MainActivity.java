9b88dbbfc2e0267f96de00b43ab8c4bbdef492e5
saddy1923 <prabhu.sarath@gmail.com>
2016-01-24 12:00:03 -0600

    Android

--- GuesstheStars/app/src/main/java/com/example/sarathkumar/guessthestars/MainActivity.java
+++ GuesstheStars/app/src/main/java/com/example/sarathkumar/guessthestars/MainActivity.java
@@ -84,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 if ( inputStream != null ) {
-
                     InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                     String receiveString = "";
