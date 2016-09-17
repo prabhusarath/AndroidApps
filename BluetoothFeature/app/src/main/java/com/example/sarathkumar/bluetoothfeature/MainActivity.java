ed03a840aa859a6c7078efc89d07d7f9db08344a
saddy1923 <prabhu.sarath@gmail.com>
2016-09-16 22:18:08 -0500

    Bluetooth Feature

--- BluetoothFeature/app/src/main/java/com/example/sarathkumar/bluetoothfeature/MainActivity.java
+++ BluetoothFeature/app/src/main/java/com/example/sarathkumar/bluetoothfeature/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sarathkumar.bluetoothfeature;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,27 +10,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    BluetoothAdapter B;
-    
+    BluetoothAdapter BTooth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        if(BTooth.isEnabled())
+        {
+            Toast.makeText(getApplicationContext(),"Bluetooth is Already On",Toast.LENGTH_LONG).show();
+        }
+
+        else
+        {
+
+            Intent i = new Intent(BTooth.ACTION_REQUEST_ENABLE);
+            startActivity(i);
+
+            if (BTooth.isEnabled())
+            {
+                Toast.makeText(getApplicationContext(),"Bluetooth is Turned On",Toast.LENGTH_LONG).show();
             }
-        });
+        }
     }
 
     @Override
